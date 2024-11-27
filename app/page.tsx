7fc2e9db95b8853fa14b1e126e926a52f566fcaa
Jessica Huang <151045619+Messyjessyyy@users.noreply.github.com>
2024-11-27 14:04:56 -0500

    hackathon

--- app/page.tsx
+++ app/page.tsx
@@ -79,13 +79,13 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 list-item">
-            Get started by editing{" "}
+            CUNY AI Schedule Builder{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>
             .
           </li>
-          <li className="list-item">CUNY AI Schedule Builder.</li>
+          <li className="list-item">CUNY AI Schedule Builder</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
@@ -102,7 +102,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Create a Schedule 
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44 button"
@@ -128,7 +128,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Rate my Professor 
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4 footer-link"
